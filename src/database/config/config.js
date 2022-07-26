5971e27ad7360b2828f65d3ec4a7b1d047c94c7c
kingsley <kingsley.davidakindele@gmail.com>
2022-07-26 14:24:05 +0100

    Fix small bug

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -12,5 +12,12 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 }
+// npx sequelize-cli db:migrate
