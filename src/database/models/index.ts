42d486868d3296df5024c53c33848f08a9249bce
Akindele Kingsley Akinyemi <kingsley.davidakindele@gmail.com>
2022-08-22 06:49:35 +0100

    hoping this is the strig issue

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const databaseUrl: string = (process.env.DATABASE_URL as string);
+const databaseUrl: string = (process.env.DATABASE_URL as string) || '';
 
 export const sequelize = new Sequelize(databaseUrl,
     {
