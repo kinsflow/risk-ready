f6091368d34abf4c589295b1a2d37fdd1be92bf4
Akindele Kingsley Akinyemi <kingsley.davidakindele@gmail.com>
2022-08-25 18:53:18 +0100

    Fix image issue

--- src/multer.ts
+++ src/multer.ts
@@ -24,7 +24,7 @@ const storage = multer.diskStorage({
     }
 })
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/png') {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
         cb(null, true)
     } else {
         cb(new Error('invalid file format'), false)
